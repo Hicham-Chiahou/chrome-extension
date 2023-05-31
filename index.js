dda3f91ddb77b39991e5a698646450cf47114738
hicham chiahou <hicham.chiahou@gmail.com>
2023-05-31 18:18:35 +0100

    setting the project

---
+++ index.js
@@ -0,0 +1,8 @@
+
+let inputBtn = document.getElementById("input-btn");
+let myLeads = []
+let inputLeads = document.getElementById("")
+
+inputBtn.addEventListener("click", function () {
+  console.log("Button clicked!");
+});
